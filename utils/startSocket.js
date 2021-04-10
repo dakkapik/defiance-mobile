7997f931737b61e374611323d9ac6f547fa931d3
BloodSport <ae86rodas@outlook.com>
2021-04-09 21:11:40 -0400

    Update startSocket.js
    added user based login

--- utils/startSocket.js
+++ utils/startSocket.js
@@ -1,16 +1,20 @@
-import { Manager } from "socket.io-client"
-
 export default function (credentials, endpoint) {
-    const manager = new Manager(endpoint)
+    return new Promise (( resolve, reject ) => {
+        const io = require("socket.io-client");
+    
+        const socket = io(endpoint)
+    
+        socket.on("connect", () => {
+            socket.emit("new-user", {id: credentials.id, role: credentials.role, store: "psq2"});
+        });
+        
+        socket.on("login-success", () => {
+            resolve(socket);
+        });
 
-    const socket = manager.socket("",{
-        auth: {
-            token: credentials
-        }
-    })
-
-    socket.on("connect", () =>{
-        console.log(socket.id)
-        socket.emit("new-user", {id: 3533, role: "manager", store:"psq2" })
+        socket.on("error", (error) => {
+            socket.disconnect();
+            reject(error);
+        })
     })
 }
