c2d984b1be9551b5ad8c72dd11ae753bb4d63a08
BloodSport <ae86rodas@outlook.com>
2021-04-09 21:11:15 -0400

    Update getCredentials.js
    added base 64 decoding to the payload of the token

--- utils/getCredentials.js
+++ utils/getCredentials.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import { Buffer } from 'buffer';
 
 export default function getCredentials (employeeId, endpoint) {
     return new Promise(( resolve, reject ) => {
       axios.get(`${endpoint}/auth/${employeeId}`)
       .then((res)=>{
-        resolve(res.data)
+        const { id, name, role } = JSON.parse(Buffer.from(res.data.split(".")[1], 'base64'))
+        resolve({token: res.data, id, name, role})
       })
       .catch(err => reject(err))
     })
