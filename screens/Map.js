8d48da5f210859b667987cffd0021a188e1a0cf5
BloodSport <ae86rodas@outlook.com>
2021-04-12 14:42:48 -0400

    added finish order button

--- screens/Map.js
+++ screens/Map.js
@@ -4,17 +4,35 @@ import startSocket from "../utils/startSocket";
 import LocationAuthorization from "../utils/LocationAuthorization";
 import MapView from "../components/MapView";
 import * as Location from "expo-location";
+import mockData from "../mock/responce2.json"
 
 export default function Map({ route, navigation }) {
 
-  const { credentials, settings } = route.params
+  // const { credentials, settings } = route.params
+
+  const credentials = {
+    id: 4545,
+    name: "George",
+    role: "driver",
+    // token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDU0NSwicm9sZSI6ImRyaXZlciIsIm5hbWUiOiJHZW9yZ2UiLCJpYXQiOjE2MTgyNDEyNDh9.PAMdiAs8-ZwrH5UrlhRnIczF0mqKkaIqmry0i8hW2ZA",
+  };
+  
+  const settings =  {
+    ENDPOINT: "http://69.65.91.236:3001",
+    GPS_ACCURACY: 6,
+    STORE_LOCATION: {
+      latitude: 26.286637840478523,
+      longitude: -80.20009302407799,
+    },
+  };
 
   const [ LocationAuth, setLocationAuth ] = useState(false);
   const [ position, setPosition ] = useState(settings.STORE_LOCATION);
-  const [ directions, setDirections ] = useState([]);
+  const [ directions, setDirections ] = useState(mockData);
   const [ compass, setCompass ] = useState({});
   const [ navigate, setNavigate ] = useState(false);
   const [ socket, setSocket ] = useState(null)
+  const [ closeToMarker, setCloseToMarker ] = useState(false)
 
   const handleLocationAuthorization = () => {
     LocationAuthorization()
@@ -30,21 +48,25 @@ export default function Map({ route, navigation }) {
     .catch((err) => console.log("can't start without location permissions, activate manually", err))
   }
 
-  useEffect(() => {
-    if(socket){
-      socket.on("route", (route)=>{
-        console.log("directions received")
-        setDirections(route)
-      })
-    }
+  const handleFinishDelivery = () => {
+    console.log("delivery finished")
+  }
 
-    return () => {
-      if(socket){
-        socket.disconnect()
-        setSocket(null)
-      }
-    }
-  }, [socket])
+  // useEffect(() => {
+  //   if(socket){
+  //     socket.on("route", (route)=>{
+  //       console.log("directions received")
+  //       setDirections(route)
+  //     })
+  //   }
+
+  //   return () => {
+  //     if(socket){
+  //       socket.disconnect()
+  //       setSocket(null)
+  //     }
+  //   }
+  // }, [socket])
 
   useEffect(() => {
 
@@ -86,10 +108,25 @@ export default function Map({ route, navigation }) {
     LocationAuth ?
     <View style={styles.fullScreen}>
       <Text>LAT: { position.latitude } LNG: { position.longitude}</Text>  
-      <MapView position={ position } navigate={ navigate } compass={ compass } directions={ directions } />
-      <TouchableOpacity style={styles.startNavigationButton} onPress={()=>setNavigate(!navigate)}>
+      <MapView 
+        position={ position } 
+        navigate={ navigate } 
+        compass={ compass } 
+        directions={ directions } 
+        setCloseToMarker={ setCloseToMarker }
+      />
+      <View style={styles.mapUI}>
+      <TouchableOpacity style={styles.UIButton} onPress={()=>setNavigate(!navigate)}>
         <Text style={styles.font}>{navigate ? "STOP NAVIGATION": "START NAVIGATION"}</Text>
       </TouchableOpacity>
+      { closeToMarker ? 
+        <TouchableOpacity style={styles.UIButton} onPress={handleFinishDelivery}>
+          <Text style={styles.font}>FINISH DELIVERY</Text>
+        </TouchableOpacity>
+      :
+        null
+      }
+      </View>
     </View>
     :
     <View style={styles.fullScreen}>
@@ -120,9 +157,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  startNavigationButton: {
-    position:"absolute",
-    bottom: 60,
+  mapUI:{
+    flex: 1,
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    backgroundColor: "black"
+  },
+  UIButton: {
+    flex:1,
+    borderWidth: 2,
+    borderColor: "black",
     width: 100,
     height: 100,
     backgroundColor: "red",
